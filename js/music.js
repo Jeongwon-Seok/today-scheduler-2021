3c17992a9fc410a191657edaf1b512e802bb7824
석정원 <84588595+Jeongwon-Seok@users.noreply.github.com>
2021-08-09 01:44:52 +0900

    #11 FIN

--- js/music.js
+++ js/music.js
@@ -1,54 +1,28 @@
 const musics = [
   {
     img: "img/mu-1.jpg",
-    music: "The way to get started is to quit talking and begin doing.",
-    singer: "Walt Disney",
+    music: "Twist and Shout",
+    singer: "The Beatles",
   },
   {
-    img: "img/mu-1.jpg",
-    music: "Life is what happens when you're busy making other plans.",
-    singer: "John Lennon",
+    img: "img/mu-2.jpg",
+    music: "The Beatles' Hits",
+    singer: "The Beatles",
   },
   {
-    img: "img/mu-1.jpg",
-    music:
-      "The world is a book and those who do not travel read only one page.",
-    singer: "Saint Augustine",
+    img: "img/mu-3.jpg",
+    music: "The Beatles (No. 1)",
+    singer: "The Beatles",
   },
   {
-    img: "img/mu-1.jpg",
-    music: "Life is either a daring adventure or nothing at all.",
-    singer: "Helen Keller",
+    img: "img/mu-4.jpg",
+    music: "All My Loving",
+    singer: "The Beatles",
   },
   {
-    img: "img/mu-1.jpg",
-    music: "To Travel is to Live",
-    singer: "Hans Christian Andersen",
-  },
-  {
-    img: "img/mu-1.jpg",
-    music: "Only a life lived for others is a life worthwhile.",
-    singer: "Albert Einstein",
-  },
-  {
-    img: "img/mu-1.jpg",
-    music: "You only live once, but if you do it right, once is enough.",
-    singer: "Mae West",
-  },
-  {
-    img: "img/mu-1.jpg",
-    music: "Never go on trips with anyone you do ntot love.",
-    singer: "Hemmingway",
-  },
-  {
-    img: "img/mu-1.jpg",
-    music: "We wander for distraction, but we travel for fulfilment.",
-    singer: "Hilaire Belloc",
-  },
-  {
-    img: "img/mu-1.jpg",
-    music: "Travel expands the mind and fills the gap.",
-    singer: "Sheda Savage",
+    img: "img/mu-5.jpg",
+    music: "Long Tall Sally",
+    singer: "The Beatles",
   },
 ];
 
